a2adeebe2ac823b4b077ca9f9a400fa6d040d5c8
Keshavarzi <keshavarzi@predic8.de>
2013-01-15 15:08:58 +0100

    soa-model-distribution-1.2.2 released.

--- distribution/analyzewsdl.bat
+++ distribution/analyzewsdl.bat
@@ -1,10 +1,10 @@
 @echo off
 if not "%SOA_MODEL_HOME%" == "" goto homeSet
 set "SOA_MODEL_HOME=%cd%"
-if exist "%SOA_MODEL_HOME%\schemadiff.bat" goto homeOk
+if exist "%SOA_MODEL_HOME%\analysewsdl.bat" goto homeOk
 
 :homeSet
-if exist "%SOA_MODEL_HOME%\schemadiff.bat" goto homeOk
+if exist "%SOA_MODEL_HOME%\analysewsdl.bat" goto homeOk
 echo Please set the SOA_MODEL_HOME environment variable to point to
 echo the directory where you have extracted the SOA Model software 
 echo and put it in the your path environment variable.
@@ -18,6 +18,6 @@ set "CLASSPATH=%CLASSPATH%;%SOA_MODEL_HOME%/lib/commons-logging-1.1.1.jar"
 set "CLASSPATH=%CLASSPATH%;%SOA_MODEL_HOME%/lib/groovy-all-1.8.6.jar"
 set "CLASSPATH=%CLASSPATH%;%SOA_MODEL_HOME%/lib/commons-cli-1.2.jar"
 set "CLASSPATH=%CLASSPATH%;%SOA_MODEL_HOME%/bin"
-java  -classpath "%CLASSPATH%" org.membrane_soa.soa_model.creator.CreateSOAPRequestTemplate %1 %2 %3 %4 %5 %6
+java  -classpath "%CLASSPATH%" org.membrane_soa.soa_model.analyzer.WSDLAnalyzer %1 %2 %3 %4 %5 %6
 
 :end
--- distribution/src/org/membrane_soa/soa_model/analyzer/WSDLAnalyzer.groovy
+++ distribution/src/org/membrane_soa/soa_model/analyzer/WSDLAnalyzer.groovy
@@ -1,11 +1,25 @@
-package org.membrane_soa.soa_model.analyser
+/* Copyright 2012 predic8 GmbH, www.predic8.com
+ 
+		Licensed under the Apache License, Version 2.0 (the "License");
+		you may not use this file except in compliance with the License.
+		You may obtain a copy of the License at
+ 
+		http://www.apache.org/licenses/LICENSE-2.0
+ 
+		Unless required by applicable law or agreed to in writing, software
+		distributed under the License is distributed on an "AS IS" BASIS,
+		WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+		See the License for the specific language governing permissions and
+		limitations under the License. */
+
+package org.membrane_soa.soa_model.analyzer
 
 import groovy.xml.MarkupBuilder
 
 import com.predic8.wsdl.Definitions
 import com.predic8.wsdl.WSDLParser
 
-class WSDLAnalyser{
+class WSDLAnalyzer{
 
 	String url
 	String reportFolder = 'analysewsdl-report'
@@ -13,7 +27,7 @@ class WSDLAnalyser{
 	def builder
 
 	public static void main(String[] args) {
-		WSDLAnalyser wa = new WSDLAnalyser().start(args)
+		WSDLAnalyzer wa = new WSDLAnalyzer().start(args)
 	}
 
 	public start(args){
