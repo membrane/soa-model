ff22d9237d51e46aca66841ad20173c9de46004e
rverlind <ruben@ithena.be>
2013-12-10 06:54:55 +0100

    #168 Fixed concurrency issues in the ParserImportedSchemaCache when shared by multiple WSDLParsers

--- core/src/main/groovy/com/predic8/ParserImportedSchemaCache.groovy
+++ core/src/main/groovy/com/predic8/ParserImportedSchemaCache.groovy
@@ -1,32 +1,48 @@
 package com.predic8
 
 import com.predic8.schema.Schema
-
-import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.ConcurrentMap
+import groovy.transform.Synchronized
 
 /**
  * Thread safe imported Schema cache to be used in the {@link com.predic8.soamodel.AbstractParserContext}s
  */
 class ParserImportedSchemaCache {
 
-  final ConcurrentMap<String, Schema> importedSchemas = new ConcurrentHashMap()
+  @SuppressWarnings("GroovyUnusedDeclaration")
+  Object importedSchemasLock = new Object()
 
+
+  final Map<String, Schema> importedSchemas = [:]
+
+  @Synchronized('importedSchemasLock')
   Schema addSchema (Schema schema, String schemaKey = null) {
     String key = schemaKey ?: schema?.targetNamespace
-    importedSchemas.putIfAbsent(key, schema)
+
+    // Do not try to cache schema's without a meaningful key
+    if (!key) { return schema }
+
+    if (!importedSchemas[key]) {
+        importedSchemas[key] = schema
+    }
+    importedSchemas[key]
   }
 
+  @Synchronized('importedSchemasLock')
   Schema addSchema (Closure lazySchema, String schemaKey = null) {
     String key = schemaKey
     if (!schemaKey) {
       // Schema needs to be parsed to determine the cache key
       return addSchema((Schema)lazySchema.call())
     }
-    importedSchemas[key] ?: importedSchemas.putIfAbsent(key, (Schema)lazySchema.call())
+
+    if (!importedSchemas[key]) {
+        importedSchemas[key] = (Schema) lazySchema.call()
+    }
+    importedSchemas[key]
   }
 
-  Schema getSchema (String key) {
+  @Synchronized('importedSchemasLock')
+  synchronized Schema getSchema (String key) {
     importedSchemas[key]
   }
 
