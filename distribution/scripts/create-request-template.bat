e0b420de406bc51623f0134f533324628078c016
Lennart Buhl <git@hackmate.de>
2015-05-05 16:52:40 +0200

    improve scripts
    shorten classpath creation
    better handling of SOA_MODEL_HOME

--- distribution/scripts/create-request-template.bat
+++ distribution/scripts/create-request-template.bat
@@ -11,18 +11,6 @@ echo and put it in your path environment variable.
 goto end
 
 :homeOk
-set "SOA_CP=%CLASSPATH%"
-set "SOA_CP=%SOA_CP%;%SOA_MODEL_HOME%/lib/soa-model-distribution-${version}.jar"
-set "SOA_CP=%SOA_CP%;%SOA_MODEL_HOME%/lib/soa-model-core-${version}.jar"
-set "SOA_CP=%SOA_CP%;%SOA_MODEL_HOME%/lib/commons-codec-1.6.jar"
-set "SOA_CP=%SOA_CP%;%SOA_MODEL_HOME%/lib/httpclient-4.2.2.jar"
-set "SOA_CP=%SOA_CP%;%SOA_MODEL_HOME%/lib/httpcore-4.2.2.jar"
-set "SOA_CP=%SOA_CP%;%SOA_MODEL_HOME%/lib/commons-logging-1.1.1.jar"
-set "SOA_CP=%SOA_CP%;%SOA_MODEL_HOME%/lib/groovy-2.3.9.jar"
-set "SOA_CP=%SOA_CP%;%SOA_MODEL_HOME%/lib/groovy-xml-2.3.9.jar"
-set "SOA_CP=%SOA_CP%;%SOA_MODEL_HOME%/lib/asm-4.0.jar"
-set "SOA_CP=%SOA_CP%;%SOA_MODEL_HOME%/lib/commons-cli-1.2.jar"
-set "SOA_CP=%SOA_CP%;%SOA_MODEL_HOME%/lib/slf4j-api-1.7.9.jar"
-java  -classpath "%SOA_CP%" org.membrane_soa.soa_model.creator.CreateSOAPRequestTemplate %1 %2 %3 %4 %5 %6
+java  -classpath "%SOA_MODEL_HOME%/lib/*" org.membrane_soa.soa_model.creator.CreateSOAPRequestTemplate %1 %2 %3 %4 %5 %6
 
 :end
--- distribution/scripts/wsdldiff.sh
+++ distribution/scripts/wsdldiff.sh
@@ -1,32 +1,19 @@
 #!/bin/sh
 SCRIPTNAME=`basename $0`
-if [ -n $SOA_MODEL_HOME ]; then # check if SOA_MODEL_HOME is set
-	if [ ! -e "${SOA_MODEL_HOME}/bin/${SCRIPTNAME}" ]; then # if script can't find itself...
-		echo "Invalid SOA_MODEL_HOME directory specified"
-		exit 1
-	fi
-else # if SOA_MODEL_HOME is not set...
+if [ ! -e "${SOA_MODEL_HOME}/bin/${SCRIPTNAME}" ]; then # if script can't find itself...
 	SOA_MODEL_HOME=$PWD # ... try pwd
 	if [ ! -e "${SOA_MODEL_HOME}/bin/${SCRIPTNAME}" ]; then # if script can't find itself...
-		echo "Please set the SOA_MODEL_HOME environment variable to point to the"
-		echo "directory where you have extracted the SOA Model distribution package"
-		echo "and put it in your path environment variable."
-		exit 1
+		SOA_MODEL_HOME="$PWD/.." # ... try pwd/..
+		if [ ! -e "${SOA_MODEL_HOME}/bin/${SCRIPTNAME}" ]; then # if script can't find itself...
+			echo "Please set the SOA_MODEL_HOME environment variable to point to the"
+			echo "directory where you have extracted the SOA Model distribution package"
+			echo "and put it in your path environment variable."
+			exit 1
+		fi
 	fi
-fi
+fi # else: script can find itself, all good
 
-SOA_CP="${CLASSPATH}"
-SOA_CP="${SOA_CP}:${SOA_MODEL_HOME}/lib/soa-model-distribution-${version}.jar"
-SOA_CP="${SOA_CP}:${SOA_MODEL_HOME}/lib/soa-model-core-${version}.jar"
-SOA_CP="${SOA_CP}:${SOA_MODEL_HOME}/lib/commons-codec-1.6.jar"
-SOA_CP="${SOA_CP}:${SOA_MODEL_HOME}/lib/httpclient-4.2.2.jar"
-SOA_CP="${SOA_CP}:${SOA_MODEL_HOME}/lib/httpcore-4.2.2.jar"
-SOA_CP="${SOA_CP}:${SOA_MODEL_HOME}/lib/commons-logging-1.1.1.jar"
-SOA_CP="${SOA_CP}:${SOA_MODEL_HOME}/lib/groovy-2.3.9.jar"
-SOA_CP="${SOA_CP}:${SOA_MODEL_HOME}/lib/groovy-xml-2.3.9.jar"
-SOA_CP="${SOA_CP}:${SOA_MODEL_HOME}/lib/asm-4.0.jar"
-SOA_CP="${SOA_CP}:${SOA_MODEL_HOME}/lib/commons-cli-1.2.jar"
-SOA_CP="${SOA_CP}:${SOA_MODEL_HOME}/lib/slf4j-api-1.7.9.jar"
-java -classpath "${SOA_CP}" \
+java -classpath "${SOA_MODEL_HOME}/lib/*" \
   org.membrane_soa.soa_model.diff.WSDLDiffCLI \
   $1 $2 $3 $4 $5 $6
+
